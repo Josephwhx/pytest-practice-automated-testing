637d77fae95544c60ce35a7f6e5cf85551e7106f
Josephwhx <JosephWonghx@gmail.com>
2023-06-01 17:06:06 +0800

    pytest fixture

--- Pytest_topics/py_fixtures/conftest.py
+++ Pytest_topics/py_fixtures/conftest.py
@@ -17,3 +17,21 @@ def setup02():
     wk2 = pytest.weekdays2.copy()
     wk2.insert(0, 'thur')
     yield wk2
+
+@pytest.fixture()
+def setup04(request):
+    mon = getattr(request.module, "months")
+    print("\n in Fixture setup04")
+    print("\n Fixture Scope: " + str(request.scope))
+    print("\n Calling function: " + str(request.function.__name__))
+    mon.append("April")
+    yield mon
+
+@pytest.fixture()
+def setup05():
+    def get_structure(name):
+        if name == 'list':
+            return [1, 2, 3]
+        elif name == 'tuple':
+            return (1, 3, 4)
+    return get_structure
